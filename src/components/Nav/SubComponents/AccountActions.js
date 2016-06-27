75a223c56d4288b380b4f6b8adfee77cbbc68256
Tim Smith <tim.smith.hdg@gmail.com>
2016-06-27 01:43:53 -0700

    Signup mostly done

--- src/components/Nav/SubComponents/AccountActions.js
+++ src/components/Nav/SubComponents/AccountActions.js
@@ -28,14 +28,28 @@ export default class AccountActions extends React.Component {
     this.setState({ loggedOut: true })
   }
 
+  handleSignup(e) {
+    e.preventDefault()
+
+    // var email = this.refs.email
+    // var password = this.refs.password
+    // var user = this.refs.username
+    //
+    // this.setState({email, password, user})
+
+    this.setState({ loggedOut: false })
+
+    this.refs.signupForm.reset()
+  }
+
   handleUserChange(e) {
     const user = e.target.value;
     this.changeUser(user)
   }
 
   updateUserData(e){
-    e.preventDefault();
-    this.refs.changeUserName.reset();
+    e.preventDefault()
+    this.refs.changeUserName.reset()
   }
 
   changeUser(user) {
@@ -43,40 +57,60 @@ export default class AccountActions extends React.Component {
   }
   render () {
     // Replace with database connection
+    // SIGNIN/SIGNOUT
     var loggedOutClass = classNames({
       'logout-group hidden': this.state.loggedOut,
       'logout-group shown': !this.state.loggedOut
     });
     var loggedInClass = classNames({
-      'login-group shown': this.state.loggedOut,
+      'login-group shown hasDropdown': this.state.loggedOut,
       'login-group hidden': !this.state.loggedOut
+    });
+    // USER
+    var userLoggedOutClass = classNames({
+      'userLoggedOut-group hidden': this.state.loggedOut,
+      'userLoggedOut-group shown hasDropdown': !this.state.loggedOut
+    });
+    var userLoggedInClass = classNames({
+      'userLoggedIn-group shown': this.state.loggedOut,
+      'userLoggedIn-group hidden': !this.state.loggedOut
     });
     return (
       <section className='account-actions'>
-        <div className='account-dropdown'>
-          <button className='user--btn--dropdown'>
-            {this.state.user}
-          </button>
-          <ul className='form--dropdown'>
-            <li>Your Account</li>
-            <form ref='changeUserName' onSubmit={this.updateUserData.bind(this)}>
-              <input onChange={this.handleUserChange.bind(this)} />
-              <button type='submit'>Update</button>
-            </form>
-          </ul>
+        <div>
+          <section className={userLoggedInClass}>
+            <div className='account-dropdown'>
+              <button className='user--btn--dropdown'>
+                {this.state.user}
+              </button>
+            </div>
+          </section>
+          <section className={userLoggedOutClass}>
+            <div className='account-dropdown'>
+              <button className='user--btn--dropdown'>
+                {this.state.user}
+              </button>
+              <ul className='form--dropdown'>
+                <li>Your Account</li>
+                <li>{this.state.user}</li>
+                <li>{this.state.email}</li>
+                <li>{this.state.password}</li>
+              </ul>
+            </div>
+          </section>
         </div>
         <div className='button-group'>
           <div className={loggedInClass}>
             <button className='login--btn' onClick={this.login.bind(this)}>Login</button>
             <button className='signup--btn'>Signup</button>
             <ul className='signup--dropdown'>
-              <form ref='changeUserName' onSubmit={this.updateUserData.bind(this)}>
-                <input onChange={this.handleUserChange.bind(this)} />
-                <button type='submit'>Update</button>
+              <form ref='signupForm' onSubmit={this.handleSignup.bind(this)}>
+                <input ref='username' placeholder='Your Name' onChange={this.handleUserChange.bind(this)} />
+                <input ref='email' placeholder='Your Email' />
+                <input ref='password' placeholder='Select Password' />
+                <br />
+                <button className='signup--btn' type='submit'>Update</button>
               </form>
-              <pre>
-                {JSON.stringify(this.state, null, 2)}
-              </pre>
             </ul>
           </div>
           <div className={loggedOutClass}>
